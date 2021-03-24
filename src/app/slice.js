4e2f76599bb773e557a0ee2b260938607b418d11
FarDean <sfh1212@gmail.com>
2021-03-24 10:56:44 +0430

    added thunk function to slice.js

--- src/app/slice.js
+++ src/app/slice.js
@@ -1,15 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+// THUNKS
+const fetchAll = createAsyncThunk("GetAll", async (storageName, thunkAPI) => {
+	return await JSON.parse(localStorage.getItem(storageName));
+});
 
 export const expenseSlice = createSlice({
 	name: "expenses",
 	initialState: {
 		expenses: [],
+		loading: "idle",
 	},
 	reducers: {
 		add: (state, action) => state.expenses.push(action.payload),
 		getAll: (state, action) => (state.expenses = action.payload),
 		getOne: (state, action) => state.expenses.find(x => x.id === action.payload),
 		remove: (state, action) => state.expenses.filter(x => x.id !== action.payload),
+	},
+	extraReducers: {
+		[fetchAll.fulfilled]: (state, action) => {
+			state.expenses = action.payload;
+		},
 	},
 });
 
